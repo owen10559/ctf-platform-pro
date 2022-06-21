f2507bbecb186fcd664c418f1f699aaf1b01bf22
Owen <owen10559@qq.com>
2022-06-21 15:57:05 +0800

    init

--- main/apis.py
+++ main/apis.py
@@ -2,8 +2,9 @@ import config
 
 def start(username, training_name):
     """
-    docker-compose up -f trainings/<training_name>/docker-compose.yml -p <username>/<training_name> -d
+    docker-compose up -f trainings/<training_name>/docker-compose.yml -p <username>_<training_name> -d
     """
+
     ...
 
 def stop(username, training_name):
---
+++ readme.md
@@ -0,0 +1,10 @@
+
+
+# api说明
+## 启动一个training
+### URI
+GET /\<username\>/\<training_name\>
+\<username\>：用户名
+\<training_name\>：training的名称
+为指定用户启动某个training，返回该training的信息。若该training已存在，则返回对应的信息。
+### 返回值
