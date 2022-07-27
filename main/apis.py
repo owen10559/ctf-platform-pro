3d5306cc1344997a89dd66b67160fe448a9052b1
waterlily722 <92020563+waterlily722@users.noreply.github.com>
2022-07-27 23:51:13 +0800

    Update apis.py

--- main/apis.py
+++ main/apis.py
@@ -4,11 +4,36 @@ import os
 import redis
 import utils
 
+pool = redis.ConnectionPool(host='http:local', port=2375, decode_responses=True)
+r = redis.Redis(connection_pool=pool)
+
+
 def get_training_info(username, training_name):
     # 根据<username>和<training_name>获取对应的main容器
     # main容器的id即为 training_id
     # 从redis中读取对应的status和ttl，其中key：<training_id>，value为该training的status，该数据的ttl即为该training的ttl
     # 返回
+    ...
+
+
+def create_training(username, training_name):
+    # 通过 docker-compose up -d -f trainings/<training_name>/docker-compose.yml -p <username>_<training_name> 启动对应的training
+    # 根据<username>和<training_name>获取对应的main容器
+    # main容器的id即为 training_id
+    # 将以下数据存入redis：
+    # 用户拥有的training：key：<username>:trainings，value：[]，此处的value是一个列表，因此需要以类似于append的方式存入
+    # training_id的status及其ttl：key：<training_id>，value：1，ttl：***，ttl具体数值从config["trainings"]["ttl"]中读取
+    # 返回
+    ...
+
+
+def update_training_info(username, training_name):
+    # 通过 docker-compose *** -d -f trainings/<training_name>/docker-compose.yml -p <username>_<training_name>，启动或停止对应的training
+    # 根据<username>和<training_name>获取对应的main容器，main容器的id即为 training_id
+    # 修改其在redis中对应的status，key：<training_id>，value为对应的状态码，1表示正在运行，0表示已停止
+    # 返回
+    ...
+
 
 def remove_training(username, training_name):
     # 根据<username>和<training_name>获取对应的main容器，main容器的id即为 training_i
@@ -25,34 +50,19 @@ def remove_training(username, training_name):
     # training_id的status及其ttl：key：<training_id>，删除此条记录
         r.delete(training_id)   
     # 返回
-        return 204
+        return {training_name: None}, 204
     else:
-        return 404
-    ...
+        return "", 404
 
-def create_training(username, training_name):
-    # 通过 docker-compose up -d -f trainings/<training_name>/docker-compose.yml -p <username>_<training_name> 启动对应的training
-    # 根据<username>和<training_name>获取对应的main容器
-    # main容器的id即为 training_id
-    # 将以下数据存入redis：
-    # 用户拥有的training：key：<username>:trainings，value：[]，此处的value是一个列表，因此需要以类似于append的方式存入
-    # training_id的status及其ttl：key：<training_id>，value：1，ttl：***，ttl具体数值从config["trainings"]["ttl"]中读取
-    # 返回
-    ...
-
-def update_training_info(username, training_name):
-    # 通过 docker-compose *** -d -f trainings/<training_name>/docker-compose.yml -p <username>_<training_name>，启动或停止对应的training
-    # 根据<username>和<training_name>获取对应的main容器，main容器的id即为 training_id
-    # 修改其在redis中对应的status，key：<training_id>，value为对应的状态码，1表示正在运行，0表示已停止
-    # 返回
-    ...
 
 def get_training_config(training_name):
     # 读取对应training的config.json文件
     # 返回
     ...
 
+
 def verify_flag(training_name, flag):
     # 读取对应training的config.json文件中的flag，并返回
     # 返回
     ...
+
