97e99fec45545ed3e36a43aba9e53d36cb34d24f
Owen <owen10559@qq.com>
2022-08-12 15:21:34 +0800

    fix：修改了获取 redis 连接的方式

--- main/db.py
+++ main/db.py
@@ -1,7 +1,15 @@
 import config
 import redis
+import time
+
+# 等待 redis 容器初始化完成
+time.sleep(5)
 
 redis_conn_pool = redis.ConnectionPool(host=config.config["services"]["redis"]["host"], port=config.config["services"]["redis"]["port"], decode_responses=True)
+redis_conn = redis.Redis(connection_pool=redis_conn_pool)
+
+def get_redis_conn():
+    return redis.Redis(connection_pool=redis_conn_pool)
 
 if __name__ == '__main__':
     redis_conn = redis.Redis(connection_pool=redis_conn_pool)
